cd4ef5252adbca922fb425467d53d75a0c0f37f5
Salam Omarbagaev <salam.omarbagaev@jonesfamilybusiness.co.nz>
2019-06-14 15:21:56 +1200

    - added Spinner Component;

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import spinner from "./spinner.gif";
+
+//rcf as a shortcode
+const Spinner = () => (
+  <Fragment>
+    <img
+      src={spinner}
+      alt="loading..."
+      style={{ width: "200px", margin: "auto", display: "block" }}
+    />
+  </Fragment>
+);
+
+export default Spinner;
