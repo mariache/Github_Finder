7e299fdf95f448d888eabc667a80b34bff764588
Salam Omarbagaev <omarbagaev@gmail.com>
2020-05-08 01:57:24 +1200

    - removed gists and added email;

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import { Link } from "react-router-dom";
 import Repos from "../repos/Repos";
@@ -19,6 +19,7 @@ const User = ({ match }) => {
     company,
     avatar_url,
     location,
+    email,
     bio,
     blog,
     login,
@@ -26,14 +27,13 @@ const User = ({ match }) => {
     followers,
     following,
     public_repos,
-    public_gists,
-    hireable
+    hireable,
   } = user;
 
   if (loading) return Spinner;
 
   return (
-    <Fragment>
+    <>
       <Link to="/" className="btn btn-light">
         Back to search
       </Link>
@@ -56,49 +56,65 @@ const User = ({ match }) => {
         </div>
         <div>
           {bio && (
-            <Fragment>
+            <>
               <h3>Bio</h3>
               <p>{bio}</p>
-            </Fragment>
+            </>
           )}
-          <a href={html_url} className="btn btn-dark my-1">
+          <a href={html_url} className="btn btn-light my-1">
             Visit Github Profile
           </a>
           <ul>
             <li>
               {login && (
-                <Fragment>
+                <>
                   <strong>Username: </strong> {login}
-                </Fragment>
+                </>
               )}
             </li>
-
             <li>
               {company && (
-                <Fragment>
+                <>
                   <strong>Company: </strong> {company}
-                </Fragment>
+                </>
               )}
             </li>
-
+            <li>
+              {email && (
+                <>
+                  <strong>Email: </strong>
+                  <a href={`mailto:${email}`}>{email}</a>
+                </>
+              )}
+            </li>
             <li>
               {blog && (
-                <Fragment>
-                  <strong>Website: </strong> {blog}
-                </Fragment>
+                <>
+                  <strong>Website: </strong>
+                  <a href={blog} target="_blank" rel="noopener noreferrer">
+                    {blog}
+                  </a>
+                </>
               )}
+            </li>
+            <li>
+              <div className="card text-center">
+                <div className="badge badge-primary">
+                  Followers: {followers}
+                </div>
+                <div className="badge badge-success">
+                  Following: {following}
+                </div>
+                <div className="badge badge-danger">
+                  Public Repos: {public_repos}
+                </div>
+              </div>
             </li>
           </ul>
         </div>
       </div>
-      <div className="card text-center">
-        <div className="badge badge-primary">Followers: {followers}</div>
-        <div className="badge badge-success">Following: {following}</div>
-        <div className="badge badge-light">Public Repos: {public_repos}</div>
-        <div className="badge badge-dark">Public Gists: {public_gists}</div>
-      </div>
       <Repos repos={repos} />
-    </Fragment>
+    </>
   );
 };
 
