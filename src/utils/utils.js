681641ebb86ebd7ac344f108d0b0f0bbe9daf52a
Salam Omarbagaev <omarbagaev@gmail.com>
2020-05-07 16:14:42 +1200

    - func to get common languages' color;

---
+++ src/utils/utils.js
@@ -0,0 +1,46 @@
+export const languageColor = (lang) => {
+  switch (lang) {
+    case "JavaScript":
+      return "#f1e05a";
+    case "HTML":
+      return "#e34c26";
+    case "CSS":
+      return "#563d7c";
+    case "TypeScript":
+      return "#2b7489";
+    case "C#":
+      return "#178600";
+    case "CSS0":
+      return "#563d7c";
+    case "TSQL":
+      return "#CCCCCC";
+    case "PHP":
+      return "#4F5D95";
+    case "Go":
+      return "#00ADD8";
+    case "Python":
+      return "#3572A5";
+    case "Java":
+      return "#b07219";
+    case "Lua":
+      return "#000080";
+    case "Ruby":
+      return "#701516";
+    case "C++":
+      return "#f34b7d";
+    case "Per;":
+      return "#0298c3";
+    case "Swift":
+      return "#ffac45";
+    case "Objective-C":
+      return "#438eff";
+    case "Kotin":
+      return "#F18E33";
+    case "C":
+      return "#555555";
+    case "R":
+      return "#198CE7";
+    default:
+      return "#EDEDED";
+  }
+};
