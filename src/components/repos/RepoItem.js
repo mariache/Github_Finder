97edd4185455d4fac44e8ed9e56e8f3229bb3833
Salam Omarbagaev <omarbagaev@gmail.com>
2020-05-10 02:54:18 +1200

    - added repos updated date; - trancated description;

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -1,18 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { languageColor } from "../../utils/utils";
+import moment from "moment";
 
-const RepoItem = ({ repo: { name, language, html_url, description } }) => {
+const RepoItem = ({
+  repo: { name, language, html_url, description, updated_at },
+}) => {
+  const truncateDescription = (string) =>
+    string.length > 75 ? `${string.substring(0, 75)}...` : string;
+
   return (
     <div
       className="card repo"
       style={{
         borderLeft: `25px solid ${languageColor(language)} `,
+        position: "relative",
       }}
     >
       <div style={{ fontWeight: 600 }}>
         <i className="fab fa-buffer"></i>{" "}
-        <a href={html_url} target="_blank" rel="noopener noreferrer">
+        <a
+          href={html_url}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ wordBreak: "break-word" }}
+        >
           {name
             .toLowerCase()
             .split("-")
@@ -22,11 +34,20 @@ const RepoItem = ({ repo: { name, language, html_url, description } }) => {
       </div>
       {description && (
         <p className="text-secondary" style={{ margin: "0.5rem 0px" }}>
-          {description}
+          {truncateDescription(description)}
         </p>
       )}
       <p className="text-secondary" style={{ margin: "0.5rem 0px" }}>
         <span>{language ? language : ""}</span>
+      </p>
+      <p
+        className="text-secondary"
+        style={{
+          position: "absolute",
+          bottom: 0,
+        }}
+      >
+        <span>Last update: {moment(updated_at).format("YYYY-MM-DD")}</span>
       </p>
     </div>
   );
