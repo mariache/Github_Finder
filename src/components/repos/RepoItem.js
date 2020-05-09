7ece12f673d5b88b75988c6127853efc1629f036
Salam Omarbagaev <omarbagaev@gmail.com>
2020-05-10 03:33:50 +1200

    - added stars count: - added forked count;

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -4,7 +4,15 @@ import { languageColor } from "../../utils/utils";
 import moment from "moment";
 
 const RepoItem = ({
-  repo: { name, language, html_url, description, updated_at },
+  repo: {
+    name,
+    language,
+    html_url,
+    description,
+    updated_at,
+    stargazers_count,
+    forks_count,
+  },
 }) => {
   const truncateDescription = (string) =>
     string.length > 75 ? `${string.substring(0, 75)}...` : string;
@@ -18,7 +26,7 @@ const RepoItem = ({
       }}
     >
       <div style={{ fontWeight: 600 }}>
-        <i className="fab fa-buffer"></i>{" "}
+        <i className="fab fa-buffer"></i>
         <a
           href={html_url}
           target="_blank"
@@ -45,9 +53,27 @@ const RepoItem = ({
         style={{
           position: "absolute",
           bottom: 0,
+          display: "flex",
+          justifyContent: "space-between",
+          flexDirection: "column",
         }}
       >
-        <span>Last update: {moment(updated_at).format("YYYY-MM-DD")}</span>
+        <span>
+          <span style={{ marginRight: "1rem" }}>
+            <i className="fas fa-star" style={{ marginRight: "0.3rem" }}></i>
+            {stargazers_count}
+          </span>
+          <span>
+            <i
+              className="fas fa-code-branch"
+              style={{ marginRight: "0.3rem" }}
+            ></i>
+            {forks_count}
+          </span>
+        </span>
+        <span style={{ marginRight: "1rem" }}>
+          Last update: {moment(updated_at).format("YYYY-MM-DD")}
+        </span>
       </p>
     </div>
   );
