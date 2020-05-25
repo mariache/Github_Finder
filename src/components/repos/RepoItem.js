e986d006e5354454c5355b042eac7553c4893afb
Salam Omarbagaev <omarbagaev@gmail.com>
2020-05-25 23:23:08 +1200

    - Added FlipMove;

--- src/components/repos/RepoItem.js
+++ src/components/repos/RepoItem.js
@@ -4,6 +4,7 @@ import { truncateText, capitalizeFirstChar } from "../../utils/utils";
 import { languageColors } from "../../utils";
 import moment from "moment";
 import repo from "../../assets/images/repo.png";
+import FlipMove from "react-flip-move";
 
 const RepoItem = ({
   repo: {
@@ -17,60 +18,63 @@ const RepoItem = ({
   },
 }) => {
   return (
-    <div
-      className="repo flex-column"
-      style={{
-        borderLeft: `10px solid ${languageColors(language)} `,
-      }}
-    >
-      <div style={{ fontWeight: 600 }}>
-        <img
-          src={repo}
-          alt="repo"
-          style={{
-            height: "1.2rem",
-            width: "1.2rem",
-            verticalAlign: "middle",
-            marginRight: "0.3rem",
-          }}
-        />
-        <a
-          href={html_url}
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ wordBreak: "break-word" }}
-        >
-          {capitalizeFirstChar(name)}
-        </a>
-      </div>
-      {description && (
-        <p className="text-secondary" style={{ margin: "0.5rem 0px" }}>
-          {truncateText(description, 150)}
-        </p>
-      )}
-      <p className="text-secondary" style={{ margin: "0.5rem 0px" }}>
-        <span>{language ? language : <em>Language is not defined</em>}</span>
-      </p>
-      <div className="text-secondary flex-column">
-        <div>
-          <span style={{ marginRight: "1rem" }}>
-            <i
-              className="fas fa-star"
-              style={{ marginRight: "0.3rem", color: "#F08700" }}
-            ></i>
-            {stargazers_count}
-          </span>
-          <span>
-            <i
-              className="fas fa-code-branch"
-              style={{ marginRight: "0.3rem", color: "#00A9A5" }}
-            ></i>
-            {forks_count}
-          </span>
+    // @TODO sorting with benefits of FlipMove
+    <FlipMove typeName="ul">
+      <div
+        className="repo flex-column"
+        style={{
+          borderLeft: `10px solid ${languageColors(language)} `,
+        }}
+      >
+        <div style={{ fontWeight: 600 }}>
+          <img
+            src={repo}
+            alt="repo"
+            style={{
+              height: "1.2rem",
+              width: "1.2rem",
+              verticalAlign: "middle",
+              marginRight: "0.3rem",
+            }}
+          />
+          <a
+            href={html_url}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ wordBreak: "break-word" }}
+          >
+            {capitalizeFirstChar(name)}
+          </a>
         </div>
-        <div>Last update: {moment(updated_at).format("YYYY-MM-DD")}</div>
+        {description && (
+          <p className="text-secondary" style={{ margin: "0.5rem 0px" }}>
+            {truncateText(description, 150)}
+          </p>
+        )}
+        <p className="text-secondary" style={{ margin: "0.5rem 0px" }}>
+          <span>{language ? language : <em>Language is not defined</em>}</span>
+        </p>
+        <div className="text-secondary flex-column">
+          <div>
+            <span style={{ marginRight: "1rem" }}>
+              <i
+                className="fas fa-star"
+                style={{ marginRight: "0.3rem", color: "#F08700" }}
+              ></i>
+              {stargazers_count}
+            </span>
+            <span>
+              <i
+                className="fas fa-code-branch"
+                style={{ marginRight: "0.3rem", color: "#00A9A5" }}
+              ></i>
+              {forks_count}
+            </span>
+          </div>
+          <div>Last update: {moment(updated_at).format("YYYY-MM-DD")}</div>
+        </div>
       </div>
-    </div>
+    </FlipMove>
   );
 };
 
