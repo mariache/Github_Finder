48712cca632ced6ebe0a85239455f0f2e5a335ad
Salam Omarbagaev <omarbagaev@gmail.com>
2020-05-25 22:54:50 +1200

    - changed margin bottom on container; - conditional render for usersGrid;

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import SearchCard from "../users/SearchCard";
 import UsersGrid from "../users/UsersGrid";
+import GithubContext from "../../context/github/githubContext";
 
 const Home = () => {
+  const githubContext = useContext(GithubContext);
+
+  const { users } = githubContext;
   return (
     <>
       <SearchCard />
-      <UsersGrid />
+      {users.length > 0 && <UsersGrid />}
     </>
   );
 };
