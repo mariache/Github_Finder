9961454aa994359ad65e3ec760358f5d87bc238d
Salam Omarbagaev <omarbagaev@gmail.com>
2020-06-05 23:54:44 +1200

    - red text for alert;

--- src/context/alert/AlertState.js
+++ src/context/alert/AlertState.js
@@ -3,7 +3,7 @@ import AlertContext from "./alertContext";
 import AlertReducer from "./alertReducer";
 import { SET_ALERT, REMOVE_ALERT } from "../constants";
 
-const AlertState = props => {
+const AlertState = (props) => {
   const initialState = null;
 
   const [state, dispatch] = useReducer(AlertReducer, initialState);
@@ -11,7 +11,7 @@ const AlertState = props => {
   const setAlert = (msg, type) => {
     dispatch({
       type: SET_ALERT,
-      payload: { msg, type }
+      payload: { msg, type },
     });
     setTimeout(() => dispatch({ type: REMOVE_ALERT }), 2000);
   };
@@ -20,7 +20,7 @@ const AlertState = props => {
     <AlertContext.Provider
       value={{
         alert: state,
-        setAlert
+        setAlert,
       }}
     >
       {props.children}
